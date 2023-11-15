1c1d3e7400c42e9f62aedf16f61206abac6f5dfb
stripsior <nneedtoknow@proton.me>
2023-11-15 20:57:11 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,73 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+
+  const footerNavs = [
+    {
+        label: "Company",
+        items: [
+            {
+                href: 'javascript:void()',
+                name: 'Partners'
+            },
+            {
+                href: 'javascript:void()',
+                name: 'Blog'
+            },
+            {
+                href: 'javascript:void()',
+                name: 'Team'
+            },
+            {
+                href: 'javascript:void()',
+                name: 'Careers'
+            },
+        ],
+    },
+    {
+        label: "Resources",
+        items: [
+            {
+                href: 'javascript:void()',
+                name: 'contact'
+            },
+            {
+                href: 'javascript:void()',
+                name: 'Support'
+            },
+            {
+                href: 'javascript:void()',
+                name: 'Docs'
+            },
+            {
+                href: 'javascript:void()',
+                name: 'Pricing'
+            },
+        ],
+    },
+    {
+        label: "About",
+        items: [
+            {
+                href: 'javascript:void()',
+                name: 'Terms'
+            },
+            {
+                href: 'javascript:void()',
+                name: 'License'
+            },
+            {
+                href: 'javascript:void()',
+                name: 'Privacy'
+            },
+            {
+                href: 'javascript:void()',
+                name: 'About US'
+            },
+        ]
+    }
+]
+
   return (
     <html lang="pl">
       <body className='{inter.className} bg-blacks overflow-visible'>
