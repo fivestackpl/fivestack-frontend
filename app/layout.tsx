325868ed4d5cd8139e3d21e172a711844f6b66e5
stripsior <nneedtoknow@proton.me>
2023-11-16 19:53:05 +0100

    Usunieto logowanie z homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,72 +16,6 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
 
-  const footerNavs = [
-    {
-        label: "Company",
-        items: [
-            {
-                href: 'javascript:void()',
-                name: 'Partners'
-            },
-            {
-                href: 'javascript:void()',
-                name: 'Blog'
-            },
-            {
-                href: 'javascript:void()',
-                name: 'Team'
-            },
-            {
-                href: 'javascript:void()',
-                name: 'Careers'
-            },
-        ],
-    },
-    {
-        label: "Resources",
-        items: [
-            {
-                href: 'javascript:void()',
-                name: 'contact'
-            },
-            {
-                href: 'javascript:void()',
-                name: 'Support'
-            },
-            {
-                href: 'javascript:void()',
-                name: 'Docs'
-            },
-            {
-                href: 'javascript:void()',
-                name: 'Pricing'
-            },
-        ],
-    },
-    {
-        label: "About",
-        items: [
-            {
-                href: 'javascript:void()',
-                name: 'Terms'
-            },
-            {
-                href: 'javascript:void()',
-                name: 'License'
-            },
-            {
-                href: 'javascript:void()',
-                name: 'Privacy'
-            },
-            {
-                href: 'javascript:void()',
-                name: 'About US'
-            },
-        ]
-    }
-]
-
   return (
     <html lang="pl">
       <body className='bg-blacks overflow-visible'>
@@ -101,11 +35,8 @@ export default function RootLayout({
                             <a href="/">Strona Główna</a>
                         </li>
                         <li>
-                            <a href="/login" className="flex items-center text-gray-200 hover:text-gray-300">
-                                Zaloguj się
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-2" viewBox="0 0 20 20" fill="currentColor">
-                                    <path fillRule="evenodd" d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z" clipRule="evenodd" />
-                                </svg>
+                            <a href="/" className="flex items-center text-gray-200 hover:text-gray-300">
+                                
                             </a>
                         </li>
                     </ul>
